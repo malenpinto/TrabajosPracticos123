07bb9dbeeb4194aae34e2dee2b164595e5450907
malen <malenpinto9@gmail.com>
2024-08-21 19:04:15 -0300

    Agregar archivos de proyecto.

---
+++ TrabajosPracticos123/TPractico1/Practico1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TrabajosPracticos123.TPractico1
+{
+    partial class Practico1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textResultado = new System.Windows.Forms.TextBox();
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.BSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(47, 59);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 0;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(47, 111);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 1;
+            this.LNombre.Text = "Nombre";
+            // 
+            // textApellido
+            // 
+            this.textApellido.Location = new System.Drawing.Point(97, 56);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(166, 20);
+            this.textApellido.TabIndex = 2;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(97, 108);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(166, 20);
+            this.textNombre.TabIndex = 3;
+            // 
+            // textResultado
+            // 
+            this.textResultado.Location = new System.Drawing.Point(285, 56);
+            this.textResultado.Multiline = true;
+            this.textResultado.Name = "textResultado";
+            this.textResultado.Size = new System.Drawing.Size(166, 72);
+            this.textResultado.TabIndex = 4;
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.Location = new System.Drawing.Point(50, 187);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(94, 35);
+            this.BGuardar.TabIndex = 5;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = true;
+            this.BGuardar.Click += new System.EventHandler(this.BGuardar_Click);
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.Location = new System.Drawing.Point(169, 187);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(94, 35);
+            this.BEliminar.TabIndex = 6;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.UseVisualStyleBackColor = true;
+            this.BEliminar.Click += new System.EventHandler(this.BEliminar_Click);
+            // 
+            // BSalir
+            // 
+            this.BSalir.Location = new System.Drawing.Point(285, 187);
+            this.BSalir.Name = "BSalir";
+            this.BSalir.Size = new System.Drawing.Size(94, 35);
+            this.BSalir.TabIndex = 7;
+            this.BSalir.Text = "&Salir";
+            this.BSalir.UseVisualStyleBackColor = true;
+            this.BSalir.Click += new System.EventHandler(this.BSalir_Click);
+            // 
+            // Practico1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.BSalir);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
+            this.Controls.Add(this.textResultado);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textApellido);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LApellido);
+            this.KeyPreview = true;
+            this.Name = "Practico1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mi Primer Forms";
+            this.Load += new System.EventHandler(this.Practico1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Practico1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textResultado;
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button BEliminar;
+        private System.Windows.Forms.Button BSalir;
+    }
+}
---
+++ TrabajosPracticos123/TPractico1/Practico1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajosPracticos123.TPractico1
+{
+    public partial class Practico1 : Form
+    {
+        public Practico1()
+        {
+            InitializeComponent();
+        }
+
+        private void Practico1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BGuardar_Click(object sender, EventArgs e)
+        {
+            // Concatenar apellido y nombre
+            string result = textApellido.Text + " " + textNombre.Text;
+
+            // Mostrar el resultado en el tercer TextBox    
+            textResultado.Text = result;
+        }
+
+        private void BEliminar_Click(object sender, EventArgs e)
+        {
+            // Limpiar el TextBox multilinea
+            textResultado.Clear();
+        }
+
+        private void Practico1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Verifica si se presiona Ctrl + S
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                BSalir.PerformClick(); // Ejecuta el evento Click del botón Salir
+            }
+        }
+
+        private void BSalir_Click(object sender, EventArgs e)
+        {
+            // Cierra la aplicación
+            Application.Exit();
+        }
+    }
+}
